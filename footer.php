8d59d42eda24033a90906571fbdc94b750263954
BG <bg@hearingvoices.com>
2019-06-29 09:02:06 -0700

    Add filters to CMS viz

--- footer.php
+++ footer.php
@@ -19,9 +19,10 @@
             </form>
             <form id="category-select" class="category-select" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
                 <?php wp_dropdown_categories( 'show_count=1&taxonomy=cms&name=cms&value_field=slug&orderby=name&show_option_all=CMS:' ); ?> <input type="submit" name="submit" value="View CMS" />
-            </form> <?php get_search_form(); ?>
+            </form>
         </nav>
 		<div class="site-info">
+            <?php get_search_form(); ?>
 			<p>&copy;<?php echo date( 'Y' ); ?> <a href="https://www.rjionline.org/">Reynolds Journalism Institute</a> and <a href="https://journalism.missouri.edu/">Missouri School of Journalism</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
--- page-cms.php
+++ page-cms.php
@@ -15,13 +15,6 @@ get_header(); ?>
 </style>
 
 	<div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-
-            <?php while ( have_posts() ) : the_post(); ?>
-
-                <?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-            <?php endwhile; // End of the loop. ?>
 
             <?php
             $counts = 0;
@@ -79,14 +72,86 @@ get_header(); ?>
             }
             ?>
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-<script type="text/javascript">
-google.charts.load('current', {'packages':['table']});
-google.charts.setOnLoadCallback(drawTableOwners);
 
-function drawTableOwners() {
+
+            <h2>CMS by score</h2>
+            <p><?php echo $counts; ?> daily newspapers use these <?php echo $cmss; ?> CMSs<br>
+            Table sorted by their PageSpeed Insights average scores (2015-05 articles).</p>
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<div id="dashboard_div" style="border: 1px solid #ccc; margin-top: 1em">
+    <table class="columns">
+        <tr>
+            <td>
+                <div id="slider_div" style="padding-left: 15px"></div>
+            </td>
+            <td>
+
+
+                <p>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <div id="chart_div" style="padding-top: 15px"></div>
+            </td>
+            <td>
+                <div id="table_div" style="padding-top: 30px"></div>
+                <p style="font-size: 0.8rem;">( Average: <span id="avg"></span> / Count: <span id="cnt"></span> / Sum: <span id="sum"></span>)
+            </td>
+        </tr>
+    </table>
+</div>
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['corechart', 'table', 'controls']});
+    google.charts.setOnLoadCallback(drawMainDashboard);
+
+    function drawMainDashboard() {
+        var dashboard = new google.visualization.Dashboard(
+            document.getElementById('dashboard_div'));
+
+        // Dashboard filter: slider.
+        var sliderPapers = new google.visualization.ControlWrapper({
+            'controlType': 'NumberRangeFilter',
+            'containerId': 'slider_div',
+            'options': {
+                'filterColumnIndex': 1,
+                'ui': {
+                    'labelStacking': 'vertical',
+                    'label': 'Filter by papers:',
+                    'format':  { 'pattern':'#,###' },
+                }
+
+            }
+        });
+
+        var pie = new google.visualization.ChartWrapper({
+            'chartType': 'PieChart',
+            'containerId': 'chart_div',
+            'options': {
+                'width': 300,
+                'height': 300,
+                'legend': 'none',
+                'chartArea': {'left': 15, 'top': 15, 'right': 0, 'bottom': 0},
+                'pieSliceText': 'label'
+                },
+            'view': {'columns': [0, 1]}
+        });
+
+        var table = new google.visualization.ChartWrapper({
+            'chartType': 'Table',
+            'containerId': 'table_div',
+            'options': {
+                sortColumn: 7,
+                sortAscending: false,
+                showRowNumber: true,
+                 width: '100%',
+                height: '100%',
+            }
+        });
+
     var data = new google.visualization.DataTable();
-        data.addColumn('string', 'CMS');
+        data.addColumn('string', 'Owner Group');
         data.addColumn('number', 'Papers');
         data.addColumn('number', 'Dom');
         data.addColumn('number', 'Requests');
@@ -98,39 +163,64 @@ function drawTableOwners() {
 <?php echo $rows; ?>
     ]);
 
-    // Print number with separators for Ks, Ms, etc.; apply to specified columns
-    // var numsepFormat = new google.visualization.NumberFormat({pattern: '#,###'});
-    // numsepFormat.format(data, 3); // Circulation
-    // numsepFormat.format(data, 4); // Rank
+        // Print number with to one decimal place; apply to specified columns
+        var numdecFormat = new google.visualization.NumberFormat({fractionDigits: 1});
+        numdecFormat.format(data, 2);
+        numdecFormat.format(data, 3);
+        numdecFormat.format(data, 4);
+        numdecFormat.format(data, 5);
+        numdecFormat.format(data, 6);
+        numdecFormat.format(data, 7);
 
-    // Print number with to one decimal place; apply to specified columns
-    var numdecFormat = new google.visualization.NumberFormat({fractionDigits: 1});
-    numdecFormat.format(data, 2);
-    numdecFormat.format(data, 3);
-    numdecFormat.format(data, 4);
-    numdecFormat.format(data, 5);
-    numdecFormat.format(data, 6);
-    numdecFormat.format(data, 7);
+        dashboard.bind([sliderPapers], [pie, table]);
+        dashboard.draw(data);
 
-    var options = {
-        sortColumn: 7,
-        sortAscending: false,
-        showRowNumber: true,
-        width: '100%',
-        height: '100%',
-    };
+        // get average and sum
+        google.visualization.events.addListener(table, 'ready', function () {
+            var group = google.visualization.data.group(table.getDataTable(), [{
+                // we need a key column to group on, but since we want all rows grouped into 1,
+                // then it needs a constant value
+                column: 0,
+                type: 'number',
+                modifier: function () {
+                    return 1;
+                }
+            }], [{
+                // get the average
+                column: 1,
+                label: 'Average',
+                type: 'number',
+                aggregation: google.visualization.data.avg
+            }, {
+                // get the sum
+                column: 1,
+                label: 'Count',
+                type: 'number',
+                aggregation: google.visualization.data.count
+            }, {
+                // get the sum
+                column: 1,
+                label: 'Sum',
+                type: 'number',
+                aggregation: google.visualization.data.sum
+            }]);
+            document.getElementById('avg').innerHTML = group.getValue(0, 1).toFixed(1);
+            document.getElementById('cnt').innerHTML = group.getValue(0, 2);
+            document.getElementById('sum').innerHTML = group.getValue(0, 3);
+        });
 
-    var table = new google.visualization.Table(document.getElementById('table_cms'));
 
-    table.draw(data, options);
-}
+    }
+
 </script>
 
-            <h2>CMS by score</h2>
-            <p>A total of <?php echo $counts; ?> daily newspapers use these <?php echo $cmss; ?> CMSs. This table lists their PageSpeed Insights (Google) results averages for 2015-05 articles.</p>
-            <figure id="table_cms"></figure>
+        <main id="main" class="site-main" role="main">
 
-            <hr>
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+            <?php endwhile; // End of the loop. ?>
 
             <h2>Detailed results</h2>
             <p>PageSpeed Insights results averages for the daily newspapers using these CMSs:
@@ -171,6 +261,10 @@ function drawTableOwners() {
         </main><!-- #main -->
 
 	</div><!-- #primary -->
+
+    <pre>
+        <?php print_r( $pubs_data ); ?>
+    </pre>
 
 <?php // get_sidebar(); ?>
 <!-- =file: page-sitemap -->
